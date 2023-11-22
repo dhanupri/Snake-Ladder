3c9774cc6a3941330e1d78e60fcbec1b2973c440
unknown <dhanupriasokan@gmail.com>
2023-11-22 21:22:32 +0530

    usecase1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,27 @@
+
+
+public class SnakeAndLadder {
+    public static  int board_size=100;
+    public static int[] ladder_start={1,4,9,21,28,51,72,80};
+    public static int[] ladder_end={38,14,31,42,85,67,91,99};
+    public static int[] snake_head={17,54,62,64,87,92,95,98};
+    public static int[] snake_tail={7,19,34,36,60,73,75,79};
+    public static void main(String[] args){
+        int player1Position=0;
+        int player2Position=0;
+        int player1Dice=0;
+        int player2Dice=0;
+
+       
+
+
+
+
+
+
+
+
+
+
+    }
+}
